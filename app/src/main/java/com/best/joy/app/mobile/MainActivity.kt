ea0a230c853255efcfa046b714bf6bfe6cfe9100
Syed Minhal Nadeem <minhalnadeem@gmail.com>
2021-03-01 16:58:52 +0500

    Status bar color changes and clearing cache on closing app

--- app/src/main/java/com/best/joy/app/mobile/MainActivity.kt
+++ app/src/main/java/com/best/joy/app/mobile/MainActivity.kt
@@ -1,29 +1,59 @@
 package com.best.joy.app.mobile
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.MainTheme);
+        setTheme(R.style.MainTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        webView.webViewClient = MyBrowser()
-        webView.settings.loadsImagesAutomatically = true
-        webView.settings.javaScriptEnabled = true
-        webView.settings.domStorageEnabled = true
+        webView.settings.apply {
+            loadsImagesAutomatically = true
+            javaScriptEnabled = true
+            domStorageEnabled = true
+        }
+
         webView.loadUrl("http://couryli.xyz/landing.php?top=casino&top=10&app")
 
         refresh.setOnRefreshListener {
             refresh.isRefreshing = false
             webView.reload()
         }
+    }
+
+    private fun trimCache(context: Context) {
+        try {
+            val dir: File = context.cacheDir
+            if (dir.isDirectory) {
+                deleteDir(dir)
+            }
+        } catch (e: Exception) {
+            // TODO: handle exception
+        }
+    }
+
+    private fun deleteDir(dir: File?): Boolean {
+        if (dir != null && dir.isDirectory) {
+            val children: Array<String> = dir.list()
+            for (i in children.indices) {
+                val success = deleteDir(File(dir, children[i]))
+                if (!success) {
+                    return false
+                }
+            }
+        }
+
+        // The directory is now empty so delete it
+        return dir!!.delete()
     }
 
     class MyBrowser : WebViewClient() {
@@ -35,6 +65,11 @@ class MainActivity : AppCompatActivity() {
             view.loadUrl(url)
             return true
         }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        trimCache(this)
     }
 }
 
